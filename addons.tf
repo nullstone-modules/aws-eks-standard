c65ba35a175041d85d584d1876f9ea32f5ac49a1
Brad Sickles <brad.sickles@gmail.com>
2026-04-21 16:38:57 -0400

    Fixed addons (monitoring, secrets)

--- addons.tf
+++ addons.tf
@@ -50,7 +50,7 @@ resource "aws_eks_addon" "coredns" {
 
 resource "aws_eks_addon" "node_monitoring_agent" {
   cluster_name                = aws_eks_cluster.this.name
-  addon_name                  = "amazon-eks-node-monitoring-agent"
+  addon_name                  = "eks-node-monitoring-agent"
   resolve_conflicts_on_update = "PRESERVE"
   tags                        = local.tags
 
@@ -74,28 +74,15 @@ resource "aws_eks_addon" "guardduty" {
 # Secrets Store CSI Driver + AWS Provider
 # -----------------------------------------------
 
-resource "aws_eks_addon" "secrets_store_csi" {
-  cluster_name                = aws_eks_cluster.this.name
-  addon_name                  = "secrets-store-csi-driver"
-  resolve_conflicts_on_update = "PRESERVE"
-  tags                        = local.tags
-
-  timeouts {
-    create = "30m"
-  }
-}
-
 resource "aws_eks_addon" "secrets_store_csi_aws_provider" {
   cluster_name                = aws_eks_cluster.this.name
-  addon_name                  = "aws-secrets-manager-csi-driver-provider"
+  addon_name                  = "aws-secrets-store-csi-driver-provider"
   resolve_conflicts_on_update = "PRESERVE"
   tags                        = local.tags
 
   timeouts {
     create = "30m"
   }
-
-  depends_on = [aws_eks_addon.secrets_store_csi]
 }
 
 # -----------------------------------------------
