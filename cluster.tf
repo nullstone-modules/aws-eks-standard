72ec9c9ee51641a769409839a7e4d6fedf2d257f
Brad Sickles <brad.sickles@gmail.com>
2026-04-22 11:51:45 -0400

    Allow DNS to cluster security group from pod security groups

--- cluster.tf
+++ cluster.tf
@@ -46,3 +46,27 @@ resource "aws_eks_cluster" "this" {
     aws_iam_role_policy_attachment.this_vpc,
   ]
 }
+
+locals {
+  cluster_security_group_id = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
+
+resource "aws_security_group_rule" "cluster-dns-tcp-from-private-cidr" {
+  description       = "Allow DNS over TCP from VPC private subnets (pods with pod-level SGs, nodes, etc.)"
+  security_group_id = local.cluster_security_group_id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 53
+  to_port           = 53
+  cidr_blocks       = local.private_cidrs
+}
+
+resource "aws_security_group_rule" "cluster-dns-udp-from-private-cidr" {
+  description       = "Allow DNS over UDP from VPC private subnets (pods with pod-level SGs, nodes, etc.)"
+  security_group_id = local.cluster_security_group_id
+  type              = "ingress"
+  protocol          = "udp"
+  from_port         = 53
+  to_port           = 53
+  cidr_blocks       = local.private_cidrs
+}
--- network.tf
+++ network.tf
@@ -7,6 +7,7 @@ locals {
   vpc_id             = data.ns_connection.network.outputs.vpc_id
   public_subnet_ids  = data.ns_connection.network.outputs.public_subnet_ids
   private_subnet_ids = data.ns_connection.network.outputs.private_subnet_ids
+  private_cidrs      = data.ns_connection.network.outputs.private_cidrs
 }
 
 resource "aws_ec2_tag" "cluster_subnets" {
